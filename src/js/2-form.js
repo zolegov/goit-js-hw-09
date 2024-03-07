2e9d75e14be18ea3c29db27d5b43eb8cf81e8b3c
Oleg <zazubik78@gmail.com>
2024-03-07 16:13:43 +0200

    6

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -11,14 +11,14 @@ feedbackFormInput.forEach(element => {
     localStorage.setItem(feedbackFormState, e.target.value.trim());
   });
 });
-
+ 
 feedbackForm.addEventListener('submit', evt => {
   evt.preventDefault();
  
   const isAllFieldsFilled = feedbackFormInput.every(
     element => element.value.trim() !== ''
   );    
-  console.log('isAllFieldsFilled: ', isAllFieldsFilled);
+   
   if (isAllFieldsFilled) {
     console.log(localStorage);
     localStorage.clear();
