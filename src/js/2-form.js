228305aa578094e303d8aee4aa71b5ce39dacb77
Oleg <zazubik78@gmail.com>
2024-03-07 14:37:10 +0200

    12

---
+++ src/js/2-form.js
@@ -0,0 +1,30 @@
+// const feedbackFormInput = document.querySelectorAll('.feedback-form-input');
+const feedbackForm = document.querySelector('.feedback-form');
+const feedbackFormInput = Array.from(
+  document.querySelectorAll('.feedback-form-input')
+);
+
+feedbackFormInput.forEach(element => {
+  const feedbackFormState = element.name === 'email' ? 'email' : 'message';
+  element.value = localStorage.getItem(feedbackFormState);
+
+  element.addEventListener('input', e => {
+    localStorage.setItem(feedbackFormState, e.target.value.trim());
+  });
+});
+
+feedbackForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+ 
+  const isAllFieldsFilled = feedbackFormInput.every(
+    element => element.value.trim() !== ''
+  );    
+  console.log('isAllFieldsFilled: ', isAllFieldsFilled);
+  if (isAllFieldsFilled) {
+    console.log(localStorage);
+    localStorage.clear();
+    feedbackForm.reset();
+  } else {
+    alert('Заповніть всі поля');
+  }
+});
